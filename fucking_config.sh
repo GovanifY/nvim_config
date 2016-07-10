898499a5a3bf254e28348d32eb8dd1390fa6df9b
GovanifY <govanify@protonmail.com>
2016-07-10 17:10:38 +0200

    No sudo anymore

--- fucking_config.sh
+++ fucking_config.sh
@@ -1,13 +1,13 @@
 #Not making any checks whatsoever if files exists or anything
 #c'mon I mean this would be useless anyways
 echo Hey weird man that is standing in front of this screen, ya you.
-echo This setup requires sudo and basic GNU tools. u ok w/ that?
+echo This setup requires basic GNU tools. u ok w/ that?
 read
 echo Great 'cause I'm not asking you anyways!
 
 echo So here we go, time to copy the soft shortcuts!
-sudo cp neovim.png  /usr/share/icons/neovim.png
-sudo cp neovim.desktop /usr/share/applications/neovim.desktop
+cp neovim.png  ~/.local/share/icons/neovim.png
+cp neovim.desktop ~/.local/share/applications/neovim.desktop
 
 echo Removing old config to prevent a messed up one...
 rm -rf ~/.config/nvim/
